ce01bfa6bfae0996e409e4f852b573b77c0d729b
Maksim Klyuev <maks.klyuev.2000@mail.ru>
2021-01-03 21:31:23 +0300

    file merging with replacement

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,6 +1,9 @@
 import org.apache.commons.io.FileUtils;
 
 import java.io.*;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -26,13 +29,21 @@ public class Task {
     public void mergeFiles() {
         File file = new File(MERGED_PATH + mergedCounter);
 
+        String subStr;
+        System.out.println("Введите подстроку: ");
+        Scanner scanner = new Scanner(System.in);
+        subStr = scanner.nextLine();
+        System.out.println(subStr);
+        String regex = "^.+" + subStr + ".+\\n";
+
         for (int i = 0; i < 100; i++) {
             File file1 = new File(FILE_PATH + i);
 
             try {
                 String file1Str = FileUtils.readFileToString(file1) + "\n";
 
-                FileUtils.write(file, file1Str, true); // true for append
+                String importStr=file1Str.replaceAll(regex, "");
+                FileUtils.write(file, importStr, true); // true for append
             } catch (IOException e) {
                 e.printStackTrace();
             }
